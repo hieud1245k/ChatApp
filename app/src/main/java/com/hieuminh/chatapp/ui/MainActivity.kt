8c2e72040d607f4f68afb42734f5cdd17a1e4834
Nguyễn Minh Hiếu <hieuminh22490@gmail.com>
2021-03-31 15:47:35 +0700

    Connect firebase, Create bottom navigation view in main activity UI

--- app/src/main/java/com/hieuminh/chatapp/ui/MainActivity.kt
+++ app/src/main/java/com/hieuminh/chatapp/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.hieuminh.chatapp.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hieuminh.chatapp.R
 
 class MainActivity : AppCompatActivity() {
